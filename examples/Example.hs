2f1050eed7844e5f024233c80b8788b941d3d7f0
Isaac Elliott <isaace71295@gmail.com>
2016-07-26 07:51:41 +1000

    Updated example to use new features

--- examples/Example.hs
+++ examples/Example.hs
@@ -3,6 +3,7 @@
 module Example where
 
 import Control.Monad.Except
+import Control.Monad.State
 import Control.Monad.Reader
 import Data.Foldable
 import Language.Static
@@ -20,6 +21,8 @@ data Expr
 
 instance AST Expr where
   type TypeFor Expr = Ty
+  type EnvFor Expr = [(String,Ty)]
+  type ErrorFor Expr = String
   hasType ty = do
     ty' <- infer =<< ask
     if ty == ty'
@@ -33,21 +36,28 @@ instance AST Expr where
     return TBool
   infer (Not exp) = require exp $ hasType TBool
   infer (Constant _ ty) = return ty
-  infer (Variable _ ty) = return ty
+  infer (Variable name ty) = do
+    ty <- gets $ lookup name
+    case ty of
+      Nothing -> throwError . Other $ "Variable not defined"
+      Just ty -> return ty
   infer (Dereference expr) = do
     t <- infer expr
     case t of
       TRef t' -> return t'
-      _       -> throwError InferenceError
+      t'      -> throwError . TypeMismatch $ "Can't dereference " ++ show t'
 
 data Statement
   = Statements [Statement]
   | While Expr Statement
   | IfThenElse Expr Statement Statement
   | Assign Expr Expr
+  | VarDecl String Ty (Maybe Expr)
 
 instance AST Statement where
   type TypeFor Statement = ()
+  type EnvFor Statement = [(String,Ty)]
+  type ErrorFor Statement = String
 
   hasType _ = return ()
 
@@ -62,17 +72,38 @@ instance AST Statement where
     require cond $ hasType TBool
     infer one
     infer two
+  infer (VarDecl name ty right) = do
+    modify ((name,ty) :)
+    case ty of
+      TRef ty' -> case right of
+        Just expr -> void . require expr $ hasType ty'
+        Nothing -> return ()
+      _ -> throwError . Other $ "Variable not of type TRef"
 
+-- bool x = false;
+-- int y = 0;
+-- while (x) {
+--   if (y == 2)
+--     x = !x;
+--   else
+--     y = y + 1;
+-- }
 success
-  = While
-      (Dereference . Variable "x" $ TRef TBool)
-      (Statements
-        [ IfThenElse
-            (Equals (Dereference (Variable "y" $ TRef TInt)) (Constant 2 TInt))
-            (Assign (Variable "x" $ TRef TBool) (Not . Dereference . Variable "x" $ TRef TBool))
-            (Assign (Variable "y" $ TRef TInt)
-              (Add (Dereference . Variable "y" $ TRef TInt) (Constant 1 TInt)))
-        ])
+  = Statements
+      [ VarDecl "x" (TRef TBool) (Just $ Constant 0 TBool)
+      , VarDecl "y" (TRef TInt) (Just $ Constant 0 TInt)
+      , While
+          (Dereference . Variable "x" $ TRef TBool)
+          (Statements
+            [ IfThenElse
+                (Equals (Dereference (Variable "y" $ TRef TInt)) (Constant 2 TInt))
+                (Assign
+                  (Variable "x" $ TRef TBool)
+                  (Not . Dereference . Variable "x" $ TRef TBool))
+                (Assign (Variable "y" $ TRef TInt)
+                  (Add (Dereference . Variable "y" $ TRef TInt) (Constant 1 TInt)))
+            ])
+      ]
 
 failure
   = Equals (Constant 2 TInt) (Constant 2 TBool) 
